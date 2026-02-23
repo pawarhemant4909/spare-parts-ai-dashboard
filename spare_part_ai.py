3199cd4e1589db690fa89be1ca7dc44cd2ccc1ce
pawarhemant065 <pawarhemant065@gmail.com>
2026-02-23 17:08:08 +0530

    updated app.py

--- spare_part_ai.py
+++ spare_part_ai.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 df = pd.read_csv("sales_data.csv")
