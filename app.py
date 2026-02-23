617282d819a293632c24bacb9122a61eff6e7b4f
pawarhemant065 <pawarhemant065@gmail.com>
2026-02-23 18:13:51 +0530

    added editable stock, chart and dashboard improvement

--- app.py
+++ app.py
@@ -45,3 +45,25 @@ if predication[0] > current_stock:
     st.error(":( LOW STOCK !, ORDER MORE.")
 else:
     st.success("STOCK LEVEL OK :)")
+
+import streamlit as st
+import pandas as pd
+#load sale data
+df = pd.read_csv("sales_data.csv")
+st.title("spare part stock editior")
+edited_df = st.data_editor(df,num_rows="dynamic")
+if st.button("Save Changes"):
+    edited_df.to_csv("sales_data.csv", index=False)
+    st.sucess("Stock Updated!")
+st.subheader("Add Part")
+new_part = st.text_input("Part name")
+new_stock = st.number_input("Stock Available", min_value=0)
+new_price = st.number_input("unit Price", min_value=0)
+if st.button("Add Part"):
+    if new_part:
+        df.loc[len(df)] = [new_part, new_stock, new_price]
+        df.to_csv("sales_data.csv", index=False)
+        st.success(f"{new_part} added scuessfully!")
+    else: 
+        st.error("Enter a part name!")
+
